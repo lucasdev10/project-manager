0f595e9024d745a859490f46fcc74717ed2776e9
lucasdev10 <lucas.adsjr@gmail.com>
2022-04-04 18:00:47 -0300

    Exibindo os serviços

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -8,10 +8,12 @@ import Container from "./../layout/Container";
 import ProjectForm from "./../project/ProjectForm";
 import Message from "./../layout/Message";
 import ServiceForm from "./../service/ServiceForm";
+import ServiceCard from "../service/ServiceCard";
 
 function Project() {
   const { id } = useParams();
   const [project, setProject] = useState({});
+  const [services, setServices] = useState([]);
   const [showProjectForm, setShowProjectForm] = useState(false);
   const [showServiceForm, setShowServiceForm] = useState(false);
   const [message, setMessage] = useState();
@@ -27,6 +29,7 @@ function Project() {
       .then((res) => res.json())
       .then((data) => {
         setProject(data);
+        setServices(data.services);
       })
       .catch((err) => console.log(err));
   }, [id]);
@@ -88,10 +91,12 @@ function Project() {
     })
       .then((res) => res.json())
       .then((data) => {
-        // exibir os serviços
+        setShowServiceForm(false);
       })
       .catch((err) => console.log(err));
   }
+
+  function removeService() {}
 
   function toggleProjectForm() {
     setShowProjectForm(!showProjectForm);
@@ -151,7 +156,19 @@ function Project() {
             </div>
             <h2>Serviços</h2>
             <Container customClass="start">
-              <p>Itens de serviços</p>
+              {services.length > 0 &&
+                services.map((service) => (
+                  <ServiceCard
+                    id={service.id}
+                    name={service.name}
+                    cost={service.cost}
+                    description={service.description}
+                    key={service.id}
+                    handleRemove={removeService}
+                  />
+                ))}
+
+              {services.length === 0 && <p>Não há serviços cadastrados.</p>}
             </Container>
           </Container>
         </div>
