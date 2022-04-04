f9e81237339a88e5a6b6052e5c6d730920034470
lucasdev10 <lucas.adsjr@gmail.com>
2022-04-04 16:16:10 -0300

    Página de edição de dados

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import Home from './components/pages/Home';
-import Company from './components/pages/Company';
-import Contact from './components/pages/Contact';
-import NewProject from './components/pages/NewProject';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import NewProject from "./components/pages/NewProject";
 
 import Container from "./components/layout/Container";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
-import Projects from './components/pages/Projects';
+import Projects from "./components/pages/Projects";
+import Project from "./components/pages/Project";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Switch>
-        <Container customClass="min-height" >
+        <Container customClass="min-height">
           <Route exact path="/">
             <Home />
           </Route>
@@ -29,6 +30,9 @@ function App() {
           </Route>
           <Route path="/newproject">
             <NewProject />
+          </Route>
+          <Route path="/project/:id">
+            <Project />
           </Route>
         </Container>
       </Switch>
---
+++ src/components/pages/Project.js
@@ -0,0 +1,27 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+import styles from "./Project.module.css";
+
+function Project() {
+  const { id } = useParams();
+  const [project, setProject] = useState({});
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/projects/${id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setProject(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return <p>Projeto</p>;
+}
+
+export default Project;
