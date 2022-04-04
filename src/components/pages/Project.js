4ceecffdfcbb07c7282628e80fba7a8f03044e4c
lucasdev10 <lucas.adsjr@gmail.com>
2022-04-04 18:08:59 -0300

    Removendo serviços

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -96,7 +96,32 @@ function Project() {
       .catch((err) => console.log(err));
   }
 
-  function removeService() {}
+  function removeService(id, cost) {
+    setMessage("");
+
+    const servicesUpdated = project.services.filter(
+      (service) => service.id !== id
+    );
+    const projectUpdated = project;
+    projectUpdated.services = servicesUpdated;
+    projectUpdated.cost = parseFloat(projectUpdated.cost) - parseFloat(cost);
+
+    fetch(`http://localhost:5000/projects/${projectUpdated?.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(projectUpdated),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setProject(projectUpdated);
+        setServices(servicesUpdated);
+        setMessage("Serviço removido com sucesso!");
+        setType("success");
+      })
+      .catch((err) => console.log(err));
+  }
 
   function toggleProjectForm() {
     setShowProjectForm(!showProjectForm);
